e9e0394dcfd36d0ff062eec0b2c292a8b69c7d04
JK <jk.man.kim@samsung.com>
2021-06-01 11:06:47 +0900

    [AWIBOF-3638] Fix UnitTest build break
    Signed-off-by: JK <jk.man.kim@samsung.com>
    
    Former-commit-id: 29d80b27e1c767999da5bee702a5316d965f9951

--- test/unit-tests/metafs/mai/metafs_io_api_mock.h
+++ test/unit-tests/metafs/mai/metafs_io_api_mock.h
@@ -12,11 +12,14 @@ class MockMetaFsIoApi : public MetaFsIoApi
 {
 public:
     using MetaFsIoApi::MetaFsIoApi;
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Read, (FileDescriptorType fd, void* buf), (override));
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Read, (FileDescriptorType fd, FileSizeType byteOffset, FileSizeType byteSize, void* buf), (override));
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Write, (FileDescriptorType fd, void* buf), (override));
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Write, (FileDescriptorType fd, FileSizeType byteOffset, FileSizeType byteSize, void* buf), (override));
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, SubmitIO, (MetaFsAioCbCxt * cxt), (override));
+    MOCK_METHOD(POS_EVENT_ID, Read, (FileDescriptorType fd, void* buf), (override));
+    MOCK_METHOD(POS_EVENT_ID, Read, (FileDescriptorType fd, FileSizeType byteOffset, FileSizeType byteSize, void* buf), (override));
+    MOCK_METHOD(POS_EVENT_ID, Write, (FileDescriptorType fd, void* buf), (override));
+    MOCK_METHOD(POS_EVENT_ID, Write, (FileDescriptorType fd, FileSizeType byteOffset, FileSizeType byteSize, void* buf), (override));
+    MOCK_METHOD(POS_EVENT_ID, SubmitIO, (MetaFsAioCbCxt * cxt), (override));
+    MOCK_METHOD(bool, AddArray, (std::string & arrayName), (override));
+    MOCK_METHOD(bool, RemoveArray, (std::string & arrayName), (override));
+    MOCK_METHOD(void, SetStatus, (bool isNormal), (override));
 };
 
 } // namespace pos
--- test/unit-tests/metafs/mai/metafs_management_api_mock.h
+++ test/unit-tests/metafs/mai/metafs_management_api_mock.h
@@ -12,10 +12,15 @@ class MockMetaFsManagementApi : public MetaFsManagementApi
 {
 public:
     using MetaFsManagementApi::MetaFsManagementApi;
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Mount, (), (override));
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Unmount, (), (override));
-    MOCK_METHOD(MetaFsReturnCode<POS_EVENT_ID>, Create, (), (override));
-    MOCK_METHOD(bool, IsMounted, (), (override));
+    MOCK_METHOD(POS_EVENT_ID, InitializeSystem, (std::string & arrayName, MetaStorageMediaInfoList* mediaInfoList), (override));
+    MOCK_METHOD(POS_EVENT_ID, CloseSystem, (std::string & arrayName), (override));
+    MOCK_METHOD(uint64_t, GetEpochSignature, (), (override));
+    MOCK_METHOD(MetaFsStorageIoInfoList&, GetAllStoragePartitionInfo, (), (override));
+    MOCK_METHOD(MetaLpnType, GetRegionSizeInLpn, (), (override));
+    MOCK_METHOD(POS_EVENT_ID, LoadMbr, (bool& isNPOR), (override));
+    MOCK_METHOD(bool, CreateMbr, (), (override));
+    MOCK_METHOD(MetaStorageSubsystem*, GetMss, (), (override));
+    MOCK_METHOD(void, SetStatus, (bool isNormal), (override));
 };
 
 } // namespace pos
