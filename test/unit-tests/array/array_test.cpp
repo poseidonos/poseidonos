18742d57aaed5e14be41297ddb7a867920b5cd02
Heesung Yang <hsung.yang@samsung.com>
2021-06-14 15:27:10 +0900

    [SHPOS-373] Add unit test
    Former-commit-id: 29dbe06eac435a551dbb82cc02d94d80b700e194

--- test/unit-tests/array/array_test.cpp
+++ test/unit-tests/array/array_test.cpp
@@ -422,6 +422,7 @@ TEST(Array, Delete_testIfArrayDeletedSuccessfullyWhenInputsAreValid)
     EXPECT_CALL(*mockArrDevMgr, Clear).Times(1);
     EXPECT_CALL(mockAbrControl, DeleteAbr).WillOnce(Return(0));
     EXPECT_CALL(*mockState, SetDelete).Times(1);
+    EXPECT_CALL(*mockState, IsBroken).WillOnce(Return(false));
 
     Array array("mock", NULL, &mockAbrControl, mockArrDevMgr, NULL, NULL, mockState, NULL, NULL);
 
@@ -466,6 +467,7 @@ TEST(Array, Delete_testIfArrayNotDeletedWhenArrayBootRecordFailsToBeUpdated)
     EXPECT_CALL(*mockArrDevMgr, Clear).Times(1);
     EXPECT_CALL(mockAbrControl, DeleteAbr).WillOnce(Return(ABR_FAILURE));
     EXPECT_CALL(*mockState, SetDelete).Times(0); // this should never be called
+    EXPECT_CALL(*mockState, IsBroken).WillOnce(Return(false));
 
     Array array("mock", NULL, &mockAbrControl, mockArrDevMgr, NULL, NULL, mockState, NULL, NULL);
 
@@ -1040,6 +1042,28 @@ TEST(Array, TriggerRebuild_testIfFaultyArrayDeviceDoesNotNeedToRetryAfterTrigger
     delete mockArrDev;
 
     usleep(10000); // intentionally put some jitter to avoid signal propagated from internally-spawned thread
+}
+
+TEST(Array, Shutdown_testIfFlushWorksProperlyWhenShutdownOccur)
+{
+    // Given
+    MockArrayDeviceManager* mockArrDevMgr = new MockArrayDeviceManager(NULL);
+    MockIAbrControl* mockAbrControl = new MockIAbrControl();
+    MockPartitionManager* mockPtnMgr = new MockPartitionManager("mock-array", mockAbrControl);
+
+    Array array("mock-array", NULL, mockAbrControl, mockArrDevMgr, NULL, mockPtnMgr, NULL, NULL, NULL);
+
+    EXPECT_CALL(*mockArrDevMgr, ExportToMeta).Times(1);
+    EXPECT_CALL(*mockAbrControl, SaveAbr).Times(1);
+    EXPECT_CALL(*mockPtnMgr, DeleteAll).Times(1);
+    // When
+    array.Shutdown();
+
+    // Then
+    // nothing
+
+    // Cleanup
+    delete mockAbrControl;
 }
 
 } // namespace pos
