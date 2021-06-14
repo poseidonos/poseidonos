2c288753c48dd07f0fb516fd4897d7b073e423ac
Huijeong Kim <huijeong.kim@samsung.com>
2021-06-14 15:20:56 +0900

    [AWIBOF-3206] Add ActiveStripeAddress UT
    Former-commit-id: cb6c45e3dec1d81061649bb39dea61fc518b4efd

--- test/unit-tests/journal_manager/replay/active_stripe_address_test.cpp
+++ test/unit-tests/journal_manager/replay/active_stripe_address_test.cpp
@@ -4,36 +4,73 @@
 
 namespace pos
 {
-TEST(ActiveStripeAddr, ActiveStripeAddr_)
+TEST(ActiveStripeAddr, Reset_testIfALlReseted)
 {
+    // Given
+    int volId = 0;
+    VirtualBlkAddr tail = {
+        .stripeId = 100,
+        .offset = 0};
+    StripeId wbLsid = 4;
+    ActiveStripeAddr addr(volId, tail, wbLsid);
+
+    // When
+    addr.Reset();
+
+    // Then
+    EXPECT_EQ(addr.GetTail(), UNMAP_VSA);
+    EXPECT_EQ(addr.GetWbLsid(), UNMAP_STRIPE);
+    EXPECT_EQ(addr.GetVolumeId(), INT32_MAX);
 }
 
-TEST(ActiveStripeAddr, Reset_)
+TEST(ActiveStripeAddr, UpdateWbLsid_testIfWbLsidUpdated)
 {
+    // Given
+    ActiveStripeAddr addr;
+
+    // When
+    StripeId wbLsid = 5;
+    addr.UpdateWbLsid(wbLsid);
+
+    // Then
+    EXPECT_EQ(addr.GetWbLsid(), wbLsid);
 }
 
-TEST(ActiveStripeAddr, GetWbLsid_)
+TEST(ActiveStripeAddr, UpdateTail_testIfTailUpdated)
 {
+    // Given
+    ActiveStripeAddr addr;
+
+    // When
+    VirtualBlkAddr tail = {
+        .stripeId = 200,
+        .offset = 120};
+    addr.UpdateTail(tail);
+
+    // Then
+    EXPECT_EQ(addr.GetTail(), tail);
 }
 
-TEST(ActiveStripeAddr, UpdateWbLsid_)
+TEST(ActiveStripeAddr, IsValid_testWhenTailIsValid)
 {
+    // Given
+    VirtualBlkAddr tail = {
+        .stripeId = 100,
+        .offset = 5};
+    ActiveStripeAddr addr(0, tail, 0);
+
+    // When, Then
+    EXPECT_TRUE(addr.IsValid() == true);
 }
 
-TEST(ActiveStripeAddr, GetTail_)
+TEST(ActiveStripeAddr, IsValid_testWhenTailIsNotValid)
 {
-}
+    // Given
+    VirtualBlkAddr tail = UNMAP_VSA;
+    ActiveStripeAddr addr(0, tail, 0);
 
-TEST(ActiveStripeAddr, UpdateTail_)
-{
-}
-
-TEST(ActiveStripeAddr, IsValid_)
-{
-}
-
-TEST(ActiveStripeAddr, GetVolumeId_)
-{
+    // When, Then
+    EXPECT_TRUE(addr.IsValid() == false);
 }
 
 } // namespace pos
