ea02a4f21537db39074fe08624979a79d2c2afb3
Sejun Kwon <sejun.kwon@samsung.com>
2021-06-17 18:57:14 +0000

    [AWIBOF-3799] qos throttling test for single volume
    Former-commit-id: 509f295dccbeb857374c406e83d0cc5802950da9

---
+++ test/system/qos/initiator_parse_fio_result.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+
+
+default_result_file = "/tmp/fio.qos.result"
+
+f = open(default_result_file)
+total = 0
+count = 0
+for line in f:
+    if not "bw=" in line:
+        continue
+    strings = line.split()
+    parsed_str = ""
+    for st in strings:
+        if "bw=" in st:
+            parsed_str = st.split("=")[1]
+            break
+    if ("M" in parsed_str):
+        performance = float(parsed_str.split("M")[0])
+    elif ("G" in parsed_str):
+        performance = float(parsed_str.split("G")[0]) * 1024
+    else:
+        print ("So low performance !!!!!!")
+        assert(0)
+    count = count + 1
+    total = total + performance
+
+print (int((float(total)/count)))
