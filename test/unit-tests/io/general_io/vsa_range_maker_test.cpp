86a62b373129726153cc116b984262528e151717
Sangyoon Oh <sangyoon.oh@samsung.com>
2021-06-17 15:10:22 +0900

    [AWIBOF-3771] add ut for VsaRangeMaker
    Former-commit-id: 0bb5e9f6123a3197d8e52ccedbedc7df6fb7690a

--- test/unit-tests/io/general_io/vsa_range_maker_test.cpp
+++ test/unit-tests/io/general_io/vsa_range_maker_test.cpp
@@ -1,39 +1,196 @@
+/*
+ *   BSD LICENSE
+ *   Copyright (c) 2021 Samsung Electronics Corporation
+ *   All rights reserved.
+ *
+ *   Redistribution and use in source and binary forms, with or without
+ *   modification, are permitted provided that the following conditions
+ *   are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of Intel Corporation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *   OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *   DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #include "src/io/general_io/vsa_range_maker.h"
 
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
+
+#include "src/mapper/include/mapper_const.h"
+#include "src/mapper_service/mapper_service.h"
+#include "test/unit-tests/mapper/i_vsamap_mock.h"
+using namespace pos;
+using namespace std;
+using ::testing::_;
+using ::testing::NiceMock;
+using ::testing::Return;
 
 namespace pos
 {
-TEST(VsaRangeMaker, VsaRangeMaker_)
+class VsaRangeMakerTestFixture : public ::testing::Test
 {
+public:
+    VsaRangeMakerTestFixture(void)
+    {
+    }
+
+    virtual ~VsaRangeMakerTestFixture(void)
+    {
+    }
+
+    virtual void
+    SetUp(void)
+    {
+        mockIVSAMap = new NiceMock<MockIVSAMap>;
+        ON_CALL(*mockIVSAMap, GetVSAs(_, _, _, _)).WillByDefault([](int volumeId, BlkAddr startRba, uint32_t numBlks, VsaArray& vsaArray) {
+            if (startRba <= 1)
+            {
+                vsaArray[0] = {.stripeId = 0, .offset = 0};
+                vsaArray[1] = {.stripeId = 0, .offset = 1};
+            }
+            else if (startRba == 2)
+            {
+                vsaArray[0] = {.stripeId = 0, .offset = 0};
+                vsaArray[1] = {.stripeId = 1, .offset = 0};
+            }
+            else
+            {
+                vsaArray[0] = UNMAP_VSA;
+            }
+
+            if (volumeId >= MAX_VOLUME_COUNT)
+            {
+                return -1;
+            }
+            return 0;
+        });
+
+        ON_CALL(*mockIVSAMap, GetVSAInternal(_, _, _)).WillByDefault([this](int volumeId, BlkAddr startRba, int& caller) {
+            if (this->call_count++ == 0)
+            {
+                caller = NEED_RETRY;
+            }
+            else
+            {
+                caller = OK_READY;
+            }
+            VirtualBlkAddr tmpVsa{0, 0};
+            return tmpVsa;
+        });
+        MapperServiceSingleton::Instance()->RegisterMapper(arrayName, arrayId, mockIVSAMap, nullptr, nullptr, nullptr, nullptr);
+    }
+
+    virtual void
+    TearDown(void)
+    {
+        delete mockIVSAMap;
+        MapperServiceSingleton::Instance()->UnregisterMapper(arrayName);
+    }
+
+protected:
+    int call_count{0};
+    std::string arrayName = "POSArray1";
+    const uint32_t arrayId{0};
+    const uint32_t VOLUME_ID{8};
+    NiceMock<MockIVSAMap>* mockIVSAMap;
+};
+
+TEST_F(VsaRangeMakerTestFixture, VsaRangeMaker_Constructor_FiveArguments)
+{
+    //When: Create VsaRangeMaker with mapped RBA (heap)
+    VsaRangeMaker* vrm = new VsaRangeMaker(VOLUME_ID, 0, 2, false, arrayId);
+    delete vrm;
+    //do nothing
+
+    //When: Create VsaRangeMaker with mapped RBA(stack)
+    VsaRangeMaker vrm2(VOLUME_ID, 0, 1, false, arrayId);
+    //do nothing
+
+    //When: Create VsaRangeMaker with unmapped RBA(stack)
+    VsaRangeMaker vrm3(VOLUME_ID, 3, 1, false, arrayId);
+    //do nothing
+
+    //When: Create VsaRangeMaker with invalid Volume ID(stack)
+    //Then: An exeption is thrown
+    EXPECT_THROW(VsaRangeMaker vrm4(MAX_VOLUME_COUNT, 2, 2, false, arrayId), int);
 }
 
-TEST(VsaRangeMaker, GetCount_)
+TEST_F(VsaRangeMakerTestFixture, VsaRangeMaker_CheckRetry)
 {
+    //When: Create VsaRangeMaker in GC context and VSAMap is not ready
+    VsaRangeMaker vrm(VOLUME_ID, 0, 1, true, arrayId);
+    //Then: retry is required
+    EXPECT_TRUE(vrm.CheckRetry());
+
+    //When: Create VsaRangeMaker in GC context and VSAMap is ready
+    VsaRangeMaker vrm2(VOLUME_ID, 0, 1, true, arrayId);
+    //Then: retry is not needed
+    EXPECT_FALSE(vrm2.CheckRetry());
 }
 
-TEST(VsaRangeMaker, GetVsaRange_)
+TEST_F(VsaRangeMakerTestFixture, VsaRangeMaker_GetCount)
 {
+    //When: Create VsaRangeMaker with single RBA
+    VsaRangeMaker vrm(VOLUME_ID, 0, 1, false, arrayId);
+    //Then: VSA range count should be one
+    EXPECT_EQ(vrm.GetCount(), 1);
+
+    //When: Create VsaRangeMaker with multiple RBAs mapped to contiguous VSAs
+    VsaRangeMaker vrm2(VOLUME_ID, 1, 2, false, arrayId);
+    //Then: VSA range count should be one
+    EXPECT_EQ(vrm2.GetCount(), 1);
+
+    //When: Create VsaRangeMaker with multiple RBAs mapped to non-contiguous VSAs
+    VsaRangeMaker vrm3(VOLUME_ID, 2, 2, false, arrayId);
+    //Then: VSA range count should be two
+    EXPECT_EQ(vrm3.GetCount(), 2);
 }
 
-TEST(VsaRangeMaker, CheckRetry_)
+TEST_F(VsaRangeMakerTestFixture, VsaRangeMaker_GetVsaRange)
 {
-}
+    //When: Create VsaRangeMaker with single RBA
+    VsaRangeMaker vrm(VOLUME_ID, 0, 1, false, arrayId);
+    //Then: Single VSARange is returned
+    VirtualBlks actual = vrm.GetVsaRange(0);
+    VirtualBlks exp{{.stripeId = 0, .offset = 0}, .numBlks = 1};
+    EXPECT_EQ(actual, exp);
 
-TEST(VsaRangeMaker, _Add_)
-{
-}
+    //When: Create VsaRangeMaker with multiple RBAs mapped to contiguous VSAs
+    VsaRangeMaker vrm2(VOLUME_ID, 1, 2, false, arrayId);
+    //Then: Single VSARange is returned
+    actual = vrm2.GetVsaRange(0);
+    exp = {{.stripeId = 0, .offset = 0}, .numBlks = 2};
+    EXPECT_EQ(actual, exp);
 
-TEST(VsaRangeMaker, _SetNewRange_)
-{
-}
+    //When: Create VsaRangeMaker with multiple RBAs mapped to contiguous VSAs
+    VsaRangeMaker vrm3(VOLUME_ID, 2, 2, false, arrayId);
+    //Then: Two discrete VSARanges are returned
+    actual = vrm3.GetVsaRange(0);
+    exp = {{.stripeId = 0, .offset = 0}, .numBlks = 1};
+    EXPECT_EQ(actual, exp);
 
-TEST(VsaRangeMaker, _IsContiguous_)
-{
-}
-
-TEST(VsaRangeMaker, _Cut_)
-{
+    actual = vrm3.GetVsaRange(1);
+    exp = {{.stripeId = 1, .offset = 0}, .numBlks = 1};
+    EXPECT_EQ(actual, exp);
 }
 
 } // namespace pos
