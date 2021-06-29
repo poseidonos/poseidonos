afdd9f88896401a9f4feb31e79f69e407247b650
Huijeong Kim <huijeong.kim@samsung.com>
2021-06-29 18:38:35 +0900

    [SHPOS-452] Remove whitespace
    Former-commit-id: 19a19e3c3d9b7704384cb88a9e0d0f51baf32959

--- test/system/spor/MOUNT_UNMOUNT_WITH_VOLUME_DELETE.py
+++ test/system/spor/MOUNT_UNMOUNT_WITH_VOLUME_DELETE.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     TEST_LIB.set_up(argv=sys.argv, test_name=filename)
 
     TEST_SETUP_POS.clean_bringup()
-    
+
     for i in range(0, 5):
         TEST_LOG.print_notice("Iteration {}".format(i))
         TEST_SETUP_POS.create_subsystem(arrayId, volId)
