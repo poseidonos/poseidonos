60f90d7a4991cdf65d66647de2231b2613b391a8
minjoon.ahn <minjoon.ahn@samsung.com>
2021-08-17 19:01:41 +0900

    [AWIBOF-4182] UTs for logger
    Former-commit-id: 4eec9e09f3904dc3eab737b9682f3a21ab819b9d

--- test/unit-tests/helper/string_helper_test.cpp
+++ test/unit-tests/helper/string_helper_test.cpp
@@ -1,3 +1,17 @@
 #include "src/helper/string_helper.h"
 
 #include <gtest/gtest.h>
+
+TEST(StringHelper, Trim_testIfBothLTrimAndRTrimWorksWell)
+{
+    // Given
+    string testStr = " This is a string for trim test.    ";
+
+    // When
+    string outStr = trim(testStr);
+    string expectedTrimmedStr = "This is a string for trim test.";
+    bool actual = outStr == expectedTrimmedStr;
+
+    // Then
+    ASSERT_TRUE(actual);
+}
--- test/unit-tests/logger/logger_test.cpp
+++ test/unit-tests/logger/logger_test.cpp
@@ -1,55 +1,146 @@
 #include "src/logger/logger.h"
 
 #include <gtest/gtest.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 namespace pos
 {
-TEST(Logger, Logger_)
+TEST(Logger, Logger_testWhetherTheResultOfGetlevelAndInputOfSetlevelAreTheSame)
 {
+    Logger* logger = new Logger();
+    string inputLevel = "warning";
+    string expectedOutput = inputLevel;
+    logger->SetLevel(inputLevel);
+    string actualOutput = logger->GetLevel();
+
+    bool actual = expectedOutput == actualOutput;
+
+    ASSERT_TRUE(actual);
 }
 
-TEST(Logger, IboflogWithDump_)
+TEST(Logger, SetLevel_tesIfLevelOffIsSetWhenSetTheWrongLoglevel)
 {
+    Logger* logger = new Logger();
+    string inputLevel = "wrongLevel";
+    string expectedOutput = "debug";
+    logger->SetLevel(inputLevel);
+    string actualOutput = logger->GetLevel();
+
+    bool actual = expectedOutput == actualOutput;
+
+    ASSERT_TRUE(actual);
 }
 
-TEST(Logger, Iboflog_)
+TEST(Logger, GetLogDir_testIfLogDirIsSameAsPreferences)
 {
+    Logger* logger = new Logger();
+    string dirInPreferences = "/var/log/pos/";
+    string actualOutput = logger->GetLogDir();
+
+    bool actual = dirInPreferences == actualOutput;
+
+    ASSERT_TRUE(actual);
 }
 
-TEST(Logger, SetLevel_)
+TEST(Logger, PosLog_checkIfPosLogOfLoggerWritesWell)
 {
+    // Backup existing log
+    string posLog = "/var/log/pos/pos.log";
+    string posLogBackup = "/var/log/pos/pos.log.bak.test";
+    rename(posLog.c_str(), posLogBackup.c_str());
+
+    // When
+    Logger* logger = new Logger();
+    logger->Poslog(spdlog::source_loc{__FILE__, __LINE__, __FUNCTION__}, spdlog::level::info, 9999, "test string");
+    ifstream f(posLog.c_str());
+    bool exists = f.good();
+
+    // Then
+    ASSERT_TRUE(exists);
+
+    // Rollback previous log
+    remove(posLog.c_str());
+    rename(posLogBackup.c_str(), posLog.c_str());
 }
 
-TEST(Logger, GetLevel_)
+TEST(Reporter, PosLog_checkIfPosLogOfReporterWritesWell)
 {
+    // Backup existing report
+    string posReport = "/var/log/pos/report.log";
+    string posReportBackup = "/var/log/pos/report.log.bak.test";
+    rename(posReport.c_str(), posReportBackup.c_str());
+
+    // When
+    Reporter* reporter = new Reporter();
+    reporter->Poslog(spdlog::source_loc{__FILE__, __LINE__, __FUNCTION__}, spdlog::level::info, 9999, "test string");
+    ifstream f(posReport.c_str());
+    bool exists = f.good();
+
+    // Then
+    ASSERT_TRUE(exists);
+
+    // Rollback previous report
+    remove(posReport.c_str());
+    rename(posReportBackup.c_str(), posReport.c_str());
 }
 
-TEST(Logger, ApplyFilter_)
+TEST(Logger, ApplyFilter_testIfFilterIsAppliedWellByGetPreferencesAfterApplyingTheFilter)
 {
-}
+    // Backup existing filter
+    string filterPath = "/var/log/pos/filter";
+    string filterBackup = "/var/log/pos/filter.bak.test";
+    rename(filterPath.c_str(), filterBackup.c_str());
 
-TEST(Logger, GetPreference_)
-{
-}
+    // Given
+    ofstream f;
+    f.open(filterPath, ios::out);
+    if (f.is_open())
+    {
+        string filterInclude = "include:1000-1010";
+        string filterExclude = "exclude:1005,1006";
+        f << filterInclude << endl << filterExclude;
+    }
+    f.close();
 
-TEST(Logger, _ShouldLog_)
-{
-}
+    Logger* logger = new Logger();
 
-TEST(Logger, _InitDeduplicator_)
-{
-}
+    // When
+    logger->ApplyFilter();
 
-} // namespace pos
+    // Rollback previous filter
+    remove(filterPath.c_str());
+    rename(filterBackup.c_str(), filterPath.c_str());
 
-namespace pos
-{
-TEST(Reporter, Reporter_)
-{
-}
+    // Then
+    JsonElement preferences = logger->GetPreference();
+    string jsonString = "{" + preferences.ToJson() + "}";
+    json jsonDoc = json::parse(jsonString.c_str());
 
-TEST(Reporter, Iboflog_)
-{
+    string filterIncluded = "";
+    if (jsonDoc["data"].contains("filter_included"))
+    {
+        filterIncluded = jsonDoc["data"]["filter_included"].get<string>();
+    }
+
+    string filterExcluded = "";
+    if (jsonDoc["data"].contains("filter_included"))
+    {
+        filterExcluded = jsonDoc["data"]["filter_excluded"].get<string>();
+    }
+
+    int filterEnabled = 0;
+    if (jsonDoc["data"].contains("filter_enabled"))
+    {
+        filterEnabled = jsonDoc["data"]["filter_enabled"].get<int>();
+    }
+
+    bool result = (filterEnabled == 1 && filterIncluded == "1000-1010" && filterExcluded == "1005,1006");
+    ASSERT_TRUE(result);
 }
 
 } // namespace pos
