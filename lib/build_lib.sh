cf1116bb8fb5a161b3dbe252fe52ae7dcb5c2af8
kyuho.son <kyuho.son@samsung.com>
2021-09-15 14:35:01 +0900

    [AWIBOF-4359] Support running library build script from rootdir
    It was impossible to run library build script from rootdir.
    By using the user typed name, it can set relative path to lib.
    
    Signed-off-by: kyuho.son <kyuho.son@samsung.com>

--- lib/build_lib.sh
+++ lib/build_lib.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rootdir=$(readlink -f $(dirname $0))/..
+cd $rootdir/lib
 cmake . -DSPDK_DEBUG_ENABLE=n -DUSE_LOCAL_REPO=n
 make -j 4
--- lib/build_lib_internal.sh
+++ lib/build_lib_internal.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rootdir=$(readlink -f $(dirname $0))/..
+cd $rootdir/lib
 cmake . -DSPDK_DEBUG_ENABLE=y -DUSE_LOCAL_REPO=y
 make -j 4
