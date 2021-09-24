93582735c5bcd5b303c80c3b28ca32ac3b57c424
munseop.lim <munseop.lim@samsung.com>
2021-09-24 18:19:57 +0900

    [AWIBOF-3770] remove invalid unit tests for metafs
    - removed unused and invalid unit tests for unit tests of metafs
    
    Signed-off-by: munseop.lim <munseop.lim@samsung.com>

--- test/unit-tests/metafs/unit_test/mfs_top_init_test_mock.h
+++
@@ -1,7 +0,0 @@
-#include <gmock/gmock.h>
-
-#include <list>
-#include <string>
-#include <vector>
-
-#include "src/metafs/unit_test/mfs_top_init_test.h"
--- test/unit-tests/metafs/unit_test/mfs_top_init_test_test.cpp
+++
@@ -1,3 +0,0 @@
-#include "src/metafs/unit_test/mfs_top_init_test.h"
-
-#include <gtest/gtest.h>
