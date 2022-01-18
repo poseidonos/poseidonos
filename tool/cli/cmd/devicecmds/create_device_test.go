6c4570e9b4b8e4236f9027c1cbf8a1ca13f90a18
Myoungjun Lee <jun20.lee@samsung.com>
2022-01-18 08:50:11 +0000

    [AWIBOF-5122] Change default values for device create command
    Change default values for the device create command.
    Previously, users need to type all the arguments for creating a
    device, which causes an inconvience.
    
    Add default values to --device-type (uram), --num-blocks (8388608)
    , and --block-size (512). These flags are not mandatory anymore.
    
    Signed-off-by: Myoungjun Lee <jun20.lee@samsung.com>

--- tool/cli/cmd/devicecmds/create_device_test.go
+++ tool/cli/cmd/devicecmds/create_device_test.go
@@ -33,8 +33,35 @@ func TestCreateDeviceCommandReq(t *testing.T) {
 	expected := `{"Request": {"command":"CREATEDEVICE","rid":"fromfakeclient","param":{"name":"dev0","num_blocks":512,"block_size":4096,"dev_type":"uram","numa":0}} }
 `
 
-
 	if expected != string(out) {
 		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	}
+}
+
+func TestCreateDeviceCommandReqWithDefaultArgs(t *testing.T) {
+
+	// Command creation
+	rootCmd := cmd.RootCmd
+
+	// mj: For testing, I temporarily redirect log output to buffer.
+	rescueStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	globals.IsTestingReqBld = true
+	// Execute the command to test with argument
+	testmgr.ExecuteCommand(rootCmd, "device", "create", "--device-name", "dev0", "--json-req")
+
+	w.Close()
+	out, _ := ioutil.ReadAll(r)
+	os.Stdout = rescueStdout
+
+	// TODO(mj): Currently, we compare strings to test the result.
+	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
+	expected := `{"Request": {"command":"CREATEDEVICE","rid":"fromfakeclient","param":{"name":"dev0","num_blocks":512,"block_size":8388608,"dev_type":"uram","numa":0}} }
+`
+
+	if expected != string(out) {
+		t.Errorf("Expected: %q Output: %s", expected, string(out))
 	}
 }
