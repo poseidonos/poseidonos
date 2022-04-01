fa623872ed54cd498d10a380146987c6c09640a6
Hyunjin So <hjin.so@samsung.com>
2022-04-01 09:39:24 +0900

    [AWIBOF-5765] Remove fff library

--- test/fff-mocks/pmem/pmem_mock.c
+++
@@ -1,6 +0,0 @@
-#include "pmem_mock.h"
-
-DEFINE_FFF_GLOBALS
-
-DEFINE_FAKE_VALUE_FUNC(void *, pmem_map_file, const char*, size_t, int, mode_t, size_t*, int*);
-//FAKE_VALUE_FUNC2(int, pmem_unmap, void *, size_t);
--- test/fff-mocks/pmem/pmem_mock.h
+++
@@ -1,9 +0,0 @@
-#ifndef PMEM_MOCK_H_
-#define PMEM_MOCK_H_
-
-#include <libpmem.h>
-
-#include "fff/fff.h"
-DECLARE_FAKE_VALUE_FUNC(void *, pmem_map_file, const char*, size_t, int, mode_t, size_t*, int*);
-
-#endif
