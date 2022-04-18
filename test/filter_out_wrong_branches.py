2586b2c202fe8103b461c9240d7b256e98b0d792
dh.ihm <dh.ihm@samsung.com>
2022-04-18 09:02:52 +0900

    [AWIBOF-5878] Fix run_cov compilie issue
    code changes:
    - fix complie issue

--- test/filter_out_wrong_branches.py
+++ test/filter_out_wrong_branches.py
@@ -13,7 +13,7 @@ def filter(input_file):
     for line in input_fd.readlines():
         if line.startswith("SF:"):
             last_file = line.split("SF:")[1].strip()
-            print "reading ", last_file
+            print("reading ", last_file)
             last_content = open(last_file, "r").read()
             last_content_split = last_content.split("\n")
             branch_intervals = get_if_else_while_expr_sorted_intervals(last_content)
@@ -32,7 +32,7 @@ def filter(input_file):
             continue
         splited = line.strip().split("BRDA:")[1].split(",")
         if len(splited) != 4:
-            print "Format isn't expected: ", line
+            print("Format isn't expected: ", line)
             continue
         line_num, block, branch_num, branch_count = splited
         src_line_idx = int(line_num) - 1
@@ -47,7 +47,7 @@ def filter(input_file):
             count_BRH += 1
         count_BRF += 1
         output_fd.write(line)
-    print "\nNew coverage data has been written to", output_fd.name
+    print("\nNew coverage data has been written to", output_fd.name)
     output_fd.close()
 
 
@@ -76,14 +76,14 @@ def get_if_else_while_expr_sorted_intervals(src_content):
             branch_line_end = branch_line_start + src_content[pos_start:pos_end].count("\n")
             intervals.append((branch_line_start, branch_line_end))
     intervals.sort(key=lambda x: x[0])
-    print "[(branch_start, branch_end)] = ", intervals
+    print("[(branch_start, branch_end)] = ", intervals)
     return intervals
 
 
 def main():
     if len(sys.argv) != 2:
-        print "Usage  ) ./filter_out_wrong_branches.py <coverage_file>"
-        print "Example) ./filter_out_wrong_branches.py covered_filtered.lcov"
+        print("Usage  ) ./filter_out_wrong_branches.py <coverage_file>")
+        print("Example) ./filter_out_wrong_branches.py covered_filtered.lcov")
         sys.exit(1)
 
     input_file = sys.argv[1]
