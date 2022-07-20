15e1ad98ff554bf97a31dd29a3f58d1ea04c88f7
Sanghyeok Park <sang7.park@samsung.com>
2022-07-20 17:47:46 +0000

    [AWIBOF-6647] Rocksdb Metafs refactoring - removed read-modify-write in rocksdb api - added splitted write in rocksdb metfs api - fixed script not to have hardcoded dev - fixed rocksdb configuration
    Signed-off-by: Sanghyeok Park <sang7.park@samsung.com>

--- script/reset_raid_device.sh
+++ script/reset_raid_device.sh
@@ -7,19 +7,23 @@ read temp
 
 echo "This script release RAID 1 devices (/dev/nvme0n1 and /dev/nvme1n1) which was used as RocksDB Meta File System"
 
+read -p "Enter rocksdb path : " rocksdbPath
+read -p "Enter RAID device : " raidDev
+read -p "Enter SSD first block device (ex. /dev/nvme0n1 ): " dev1
+read -p "Enter SSD second block device (ex. /dev/nvme1n1 ): " dev2
 # if device is busy, kill process
-fuser -ck /etc/pos/POSRaid
+fuser -ck {rocksdbPath}
 
 # check md status
-mdadm --detail /dev/md9
+mdadm --detail ${raidDev}
 cat /proc/mdstat
 
 # unmount 
-umount /dev/md9
+umount ${raidDev}
 
 # stop construct RAID
-mdadm --stop /dev/md9
+mdadm --stop ${raidDev}
 
 # remove RAID construct
-mdadm --zero-superblock /dev/nvme0n1
-mdadm --zero-superblock /dev/nvme1n1
+mdadm --zero-superblock ${dev1}
+mdadm --zero-superblock ${dev2}
