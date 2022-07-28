6143943d91a5b8298f723e8f3e2138750acbffe7
mjlee34 <jun20.lee@samsung.com>
2022-07-28 19:12:03 +0900

    [AWIBOF-6701] Revise testing for CLI client for CREATEARRAY command.
    Revise testing for CLI client for CREATEARRAY command.
    
    Previous tests compared output strings, which is fragile to the changes
    in the output.
    
    New tests compare the results (in Golang error object) with nil
    (error is nil on success).
    
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/arraycmds/create_array_test.go
+++ tool/cli/cmd/arraycmds/create_array_test.go
@@ -15,29 +15,12 @@ func TestCreateArrayCommandReq(t *testing.T) {
 	// Command creation
 	rootCmd := cmd.RootCmd
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-
 	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
 		"device1,device2,device3", "--spare", "devspare", "--array-name", "Array0", "--raid", "RAID5", "--json-req")
 
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"command":"CREATEARRAY","rid":"fromfakeclient","param":` +
-		`{"name":"Array0","raidtype":"RAID5","buffer":[{"deviceName":"uram0"}],` +
-		`"data":[{"deviceName":"device1"},{"deviceName":"device2"},` +
-		`{"deviceName":"device3"}],"spare":[{"deviceName":"devspare"}]}}`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
@@ -46,30 +29,12 @@ func TestCreateArrayCommandReqWithoutSpare(t *testing.T) {
 	// Command creation
 	rootCmd := cmd.RootCmd
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-
 	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
 		"device1,device2,device3", "--array-name", "Array0", "--raid", "RAID5", "--json-req")
 
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"Request": {"command":"CREATEARRAY","rid":"fromfakeclient",` +
-		`"param":{"name":"Array0","buffer":[{"deviceName":"uram0"}],` +
-		`"data":[{"deviceName":"device1"},{"deviceName":"device2"},` +
-		`{"deviceName":"device3"}],"raidtype":"RAID5"}} }
-`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
