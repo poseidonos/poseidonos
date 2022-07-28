176a9312f3b2c993ae17b85fafef004080475f4e
mjlee34 <jun20.lee@samsung.com>
2022-07-28 20:26:57 +0900

    [AWIBOF-6701] Improve tests for CLI client for CREATEARRAY command
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/arraycmds/create_array_test.go
+++ tool/cli/cmd/arraycmds/create_array_test.go
@@ -2,157 +2,95 @@ package arraycmds_test
 
 import (
 	"cli/cmd"
-	"cli/cmd/globals"
 	"cli/cmd/testmgr"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 // This testing tests if the request is created well in JSON form from the command line.
-func TestCreateArrayCommandReq(t *testing.T) {
+func TestCreateArray(t *testing.T) {
 
 	// Command creation
 	rootCmd := cmd.RootCmd
 
 	// Execute the command to test with argument
 	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
-		"device1,device2,device3", "--spare", "devspare", "--array-name", "Array0", "--raid", "RAID5", "--json-req")
+		"device1,device2,device3", "--spare", "devspare", "--array-name", "Array0", "--raid", "RAID5")
 
 	if err != nil {
 		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
-func TestCreateArrayCommandReqWithoutSpare(t *testing.T) {
+func TestCreateArrayWithoutSpare(t *testing.T) {
 
 	// Command creation
 	rootCmd := cmd.RootCmd
 
 	// Execute the command to test with argument
 	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
-		"device1,device2,device3", "--array-name", "Array0", "--raid", "RAID5", "--json-req")
+		"device1,device2,device3", "--array-name", "Array0", "--raid", "RAID5")
 
 	if err != nil {
 		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
-func TestCreateArrayCommandReqWithSpareBufferList(t *testing.T) {
+func TestCreateArrayWithSpareBufferList(t *testing.T) {
 
 	// Command creation
 	rootCmd := cmd.RootCmd
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-
 	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
-		"device1,device2,device3", "--array-name", "Array0", "--spare", "spare1,spare2,spare3,spare4", "--raid", "RAID5", "--json-req")
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
+		"device1,device2,device3", "--array-name", "Array0", "--spare", "spare1,spare2,spare3,spare4", "--raid", "RAID5")
 
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"Request": {"command":"CREATEARRAY","rid":"fromfakeclient",` +
-		`"param":{"name":"Array0","buffer":[{"deviceName":"uram0"}],` +
-		`"data":[{"deviceName":"device1"},{"deviceName":"device2"},` +
-		`{"deviceName":"device3"}],"spare":[{"deviceName":"spare1"},` +
-		`{"deviceName":"spare2"},{"deviceName":"spare3"},` +
-		`{"deviceName":"spare4"}],"raidtype":"RAID5"}} }
-`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
-func TestCreateArrayCommandReqWithNoBufferFlag(t *testing.T) {
+func TestCreateArrayWithNoBufferFlag(t *testing.T) {
 
 	// Command creation
 	rootCmd := cmd.RootCmd
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-
 	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "create", "--no-buffer", "--data-devs",
-		"device1,device2,device3", "--array-name", "Array0", "--spare", "spare1,spare2,spare3,spare4", "--raid", "RAID5", "--json-req")
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--no-buffer", "--data-devs",
+		"device1,device2,device3", "--array-name", "Array0", "--spare", "spare1,spare2,spare3,spare4", "--raid", "RAID5")
 
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"Request": {"command":"CREATEARRAY","rid":"7bf114be-7753-11ec-be08-005056adcaa2","param":{"name":"Array0","data":[{"deviceName":"device1"},{"deviceName":"device2"},{"deviceName":"device3"}],"spare":[{"deviceName":"spare1"},{"deviceName":"spare2"},{"deviceName":"spare3"},{"deviceName":"spare4"}],"raidtype":"RAID5"},"requestor":"cli"} }`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
-func TestCreateArrayCommandReqWithNoBufferError(t *testing.T) {
+func TestCreateArrayWithNoBufferError(t *testing.T) {
 
 	// Command creation
 	rootCmd := cmd.RootCmd
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-
 	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "create", "--data-devs",
-		"device1,device2,device3", "--array-name", "Array0", "--spare", "spare1,spare2,spare3,spare4", "--raid", "RAID5", "--json-req")
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--data-devs",
+		"device1,device2,device3", "--array-name", "Array0", "--spare", "spare1,spare2,spare3,spare4", "--raid", "RAID5")
 
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
+	expected := `required flag(s) "buffer" not set`
 
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"time":"2022-01-17T05:07:50.838819912Z","level":"ERROR","prefix":"-","file":"create_array.go","line":"41","message":"no buffer is specified."}`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err.Error() != expected {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
 
-func TestCreateArrayWithRAID10AndOddDataDevsCommandReq(t *testing.T) {
+func TestCreateArrayRAID10WithOddDataDevs(t *testing.T) {
 
 	// Command creation
 	rootCmd := cmd.RootCmd
 
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-
 	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
-		"device1,device2,device3", "--spare", "devspare", "--array-name", "Array0", "--raid", "RAID10", "--json-req")
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "create", "--buffer", "uram0", "--data-devs",
+		"device1,device2,device3", "--spare", "devspare", "--array-name", "Array0", "--raid", "RAID10")
 
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
+	expected := `RAID10 only supports even number of data devices.`
 
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `error: RAID10 only supports even number of data devices.
-`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err.Error() != expected {
+		t.Errorf("Expected: %q Output: %q", expected, err.Error())
 	}
 }
