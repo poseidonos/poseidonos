44937ef78460cc43a8a964601a9437e7c37876ac
mjlee34 <jun20.lee@samsung.com>
2022-08-01 18:03:23 +0900

    [AWIBOF-6726] Refactor CLI client and tests for MOUNTARRAY command
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/arraycmds/mount_array_test.go
+++ tool/cli/cmd/arraycmds/mount_array_test.go
@@ -2,37 +2,18 @@ package arraycmds_test
 
 import (
 	"cli/cmd"
-	"cli/cmd/globals"
 	"cli/cmd/testmgr"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 // This testing tests if the request is created well in JSON form from the command line.
 func TestMountArrayCommandReq(t *testing.T) {
 
-	// Command creation
 	rootCmd := cmd.RootCmd
 
-	// mj: For testing, I temporarily redirect log output to buffer.
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "mount", "--array-name", "Array0", "--json-req")
 
-	globals.IsTestingReqBld = true
-	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "mount", "--array-name", "Array0", "--json-req")
-
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"command":"MOUNTARRAY","rid":"fromCLI","param":{"name":"Array0"}}`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
