8db0bb1b1b1125f73ed60cc510ee6e8b593da4a4
mjlee34 <jun20.lee@samsung.com>
2022-08-01 18:12:35 +0900

    [AWIBOF-6727] Refactor CLI client and tests for REMOVESPARE command
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/arraycmds/remove_spare_test.go
+++ tool/cli/cmd/arraycmds/remove_spare_test.go
@@ -2,38 +2,17 @@ package arraycmds_test
 
 import (
 	"cli/cmd"
-	"cli/cmd/globals"
 	"cli/cmd/testmgr"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 // This testing tests if the request is created well in JSON form from the command line.
-func TestRemoveSpareCommandReq(t *testing.T) {
-
-	// Command creation
+func TestRemoveSpare(t *testing.T) {
 	rootCmd := cmd.RootCmd
 
-	// mj: For testing, I temporarily redirect log output to buffer.
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
+	_, err := testmgr.ExecuteCommand(rootCmd, "array", "rmspare", "--spare", "device0", "--array-name", "Array0", "--json-req")
 
-	globals.IsTestingReqBld = true
-	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "array", "rmspare", "--spare", "device0", "--array-name", "Array0", "--json-req")
-
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"command":"REMOVEDEVICE","rid":"fromCLI",` +
-		`"param":{"array":"Array0","spare":[{"deviceName":"device0"}]}}`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
