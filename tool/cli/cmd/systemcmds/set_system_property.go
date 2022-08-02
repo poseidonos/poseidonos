59f709aa8803a47d3e7a1c174eebcd6869213505
mjlee34 <jun20.lee@samsung.com>
2022-08-02 15:53:51 +0900

    [AWIBOF-6733] Refactor CLI client and test for SETSYSTEMPROPERTY command
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/systemcmds/set_system_property.go
+++ tool/cli/cmd/systemcmds/set_system_property.go
@@ -5,9 +5,8 @@ import (
 	"cli/cmd/displaymgr"
 	"cli/cmd/globals"
 	"cli/cmd/grpcmgr"
-	"cli/cmd/socketmgr"
+	"fmt"
 
-	"github.com/labstack/gommon/log"
 	"github.com/spf13/cobra"
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -28,44 +27,47 @@ Syntax:
 Example (To set the impact of rebuilding process on the I/O performance to low):
 	poseidonos-cli system set-property --rebuild-impact lowest
           `,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
 		// TODO(mj): now the message format is for REBUILDPERFIMPACT only.
 		// The message format should be extended for other properties also.
 
 		var command = "REBUILDPERFIMPACT"
-		uuid := globals.GenerateUUID()
+		req, buildErr := buildSetSystemPropertyReq(command)
+		if buildErr != nil {
+			fmt.Printf("failed to build request: %v", buildErr)
+			return buildErr
+		}
 
-		param := &pb.SetSystemPropertyRequest_Param{Level: set_system_property_level}
-		req := &pb.SetSystemPropertyRequest{Command: command, Rid: uuid, Requestor: "cli", Param: param}
-		reqJSON, err := protojson.Marshal(req)
+		reqJson, err := protojson.Marshal(req)
 		if err != nil {
-			log.Fatalf("failed to marshal the protobuf request: %v", err)
+			fmt.Printf("failed to marshal the protobuf request: %v", err)
+			return err
+		}
+		displaymgr.PrintRequest(string(reqJson))
+
+		res, gRpcErr := grpcmgr.SendSetSystemProperty(req)
+		if gRpcErr != nil {
+			globals.PrintErrMsg(gRpcErr)
+			return gRpcErr
 		}
 
-		displaymgr.PrintRequest(string(reqJSON))
-
-		// Do not send request to server and print response when testing request build.
-		if !(globals.IsTestingReqBld) {
-			var resJSON string
-
-			if globals.EnableGrpc == false {
-				resJSON = socketmgr.SendReqAndReceiveRes(string(reqJSON))
-			} else {
-				res, err := grpcmgr.SendSetSystemPropertyRpc(req)
-				if err != nil {
-					globals.PrintErrMsg(err)
-					return
-				}
-				resByte, err := protojson.Marshal(res)
-				if err != nil {
-					log.Fatalf("failed to marshal the protobuf response: %v", err)
-				}
-				resJSON = string(resByte)
-			}
-
-			displaymgr.PrintResponse(command, resJSON, globals.IsDebug, globals.IsJSONRes, globals.DisplayUnit)
+		printErr := displaymgr.PrintProtoResponse(command, res)
+		if printErr != nil {
+			fmt.Printf("failed to print the response: %v", printErr)
+			return printErr
 		}
+
+		return nil
 	},
+}
+
+func buildSetSystemPropertyReq(command string) (*pb.SetSystemPropertyRequest, error) {
+	uuid := globals.GenerateUUID()
+
+	param := &pb.SetSystemPropertyRequest_Param{Level: set_system_property_level}
+	req := &pb.SetSystemPropertyRequest{Command: command, Rid: uuid, Requestor: "cli", Param: param}
+
+	return req, nil
 }
 
 // Note (mj): In Go-lang, variables are shared among files in a package.
--- tool/cli/cmd/systemcmds/set_system_property_test.go
+++ tool/cli/cmd/systemcmds/set_system_property_test.go
@@ -2,38 +2,16 @@ package systemcmds_test
 
 import (
 	"cli/cmd"
-	"cli/cmd/globals"
 	"cli/cmd/testmgr"
-	"io/ioutil"
-	"os"
 	"testing"
 )
 
 // This testing tests if the request is created well in JSON form from the command line.
 func TestSetSystemPropertyCommandReq(t *testing.T) {
-
-	// Command creation
 	rootCmd := cmd.RootCmd
 
-	// mj: For testing, I temporarily redirect log output to buffer.
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-
-	globals.IsTestingReqBld = true
-	// Execute the command to test with argument
-	testmgr.ExecuteCommand(rootCmd, "system", "set-property", "--rebuild-impact", "lowest", "--json-req")
-
-	w.Close()
-	out, _ := ioutil.ReadAll(r)
-	os.Stdout = rescueStdout
-
-	// TODO(mj): Currently, we compare strings to test the result.
-	// This needs to change. i) Parsing the JSON request and compare each variable with desired values.
-	expected := `{"command":"REBUILDPERFIMPACT","rid":"fromfakeclient",` +
-		`"param":{"level":"low"}}`
-
-	if expected != string(out) {
-		t.Errorf("Expected: %q Output: %q", expected, string(out))
+	_, err := testmgr.ExecuteCommand(rootCmd, "system", "set-property", "--rebuild-impact", "lowest")
+	if err != nil {
+		t.Errorf("Expected: nil Output: %q", err.Error())
 	}
 }
