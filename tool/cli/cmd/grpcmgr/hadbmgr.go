712469163f5ffbb5dc5d2f8d25fb5c3610d5b967
mjlee34 <jun20.lee@samsung.com>
2022-08-03 15:17:25 +0900

    [AWIBOF-6740] Add a node name to CLI as a global flag
    Add a node name (for high-availbaility) to CLI as a global flag.
    
    When the users want to send a CLI command to a specific node,
    this commit allows them to specify the name of the node using the --node flag.
    
    When the CLI client executes the command, it fetches the IP address to the node
    by looking up the database having the information about the node.
    
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/grpcmgr/hadbmgr.go
+++ tool/cli/cmd/grpcmgr/hadbmgr.go
@@ -12,6 +12,7 @@ import (
 const listNodeQuery = `SELECT "name","ip","lastseen" FROM "node"`
 const listVolumeQuery = `SELECT "id","name","node_name","array_name","size","lastseen" FROM "volume"`
 const listReplicationQuery = `SELECT "id","source_volume_id","source_wal_volume_id","destination_volume_id","destination_wal_volume_id" FROM "replication"`
+const getIpv4Query = `SELECT "ip" FROM "node" WHERE "name"='%s'`
 
 func SendListNode(req *pb.ListNodeRequest) (*pb.ListNodeResponse, error) {
 
@@ -255,4 +256,43 @@ func GetHaDbInfo() (string, string, string, string, string, error) {
 	}
 
 	return host, port, username, password, dbname, nil
+}
+
+func GetIpv4(nodeName string) (string, error) {
+	db, err := OpenHaDb()
+	if err != nil {
+		return "", errors.New("an error occured while opening ha database: " + err.Error())
+	}
+
+	// close database
+	defer db.Close()
+	// check db
+	err = db.Ping()
+	if err != nil {
+		return "", errors.New("an error occured while ping to ha database: " + err.Error())
+	}
+
+	query := fmt.Sprintf(getIpv4Query, nodeName)
+	rows, err := db.Query(query)
+	if err != nil {
+		return "", errors.New("an error occured while query to ha database: " + err.Error())
+	}
+
+	defer rows.Close()
+
+	var (
+		NodegRpcIp   string = ""
+		NodegRpcPort string = globals.GrpcPort
+	)
+
+	for rows.Next() {
+		err = rows.Scan(&NodegRpcIp)
+		if err != nil {
+			return "", errors.New("an error occured while scanning ha database: " + err.Error())
+		}
+	}
+
+	gRpcAddress := fmt.Sprintf("%s:%s", NodegRpcIp, NodegRpcPort)
+
+	return gRpcAddress, nil
 }
