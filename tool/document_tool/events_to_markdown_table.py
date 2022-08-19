95785b885fb0966dd41007dd4e72279571e62723
mjlee34 <jun20.lee@samsung.com>
2022-08-19 12:34:59 +0900

    [AWIBOF-6773] Auto-generate a markdown document from pos_event.yaml
    Add an auto-generation a markdown document from pos_event.yaml.
    The document will be generated automatically every PoseidonOS build.
    
    Add package dependencies and a script for the auto-generation.
    
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

---
+++ tool/document_tool/events_to_markdown_table.py
@@ -0,0 +1,15 @@
+import io
+import yaml
+from markdownTable import markdownTable 
+
+with open('../../src/event/pos_event.yaml', 'r') as stream:
+    try:
+        events = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+table = markdownTable(events["Root"]["Event"]).setParams(row_sep = 'markdown', quote = False).getMarkdown()
+
+event_markdown_file = open("../../doc/troubleshooting/events.md", "w")
+event_markdown_file.write(table)
+event_markdown_file.close()
