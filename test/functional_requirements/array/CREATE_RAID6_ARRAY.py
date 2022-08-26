e765a77e427de9165b927b751e40b0280f9368a9
Junghyun Hong <jh34.hong@samsung.com>
2022-08-26 10:03:38 +0000

    [AWIBOF-6677] Add functional test for RAID6
    Add python funtional test for RAID6
    
    Add array, volume, and simple I/O test for RAID6
    
    Signed-off-by: Junghyun Hong <jh34.hong@samsung.com>

---
+++ test/functional_requirements/array/CREATE_RAID6_ARRAY.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import sys
+sys.path.append("../")
+sys.path.append("../../system/lib/")
+sys.path.append("../device_management/")
+
+import json_parser
+import pos
+import cli
+import api
+import SCAN_DEV_BASIC
+
+DATA = "unvme-ns-0,unvme-ns-1,unvme-ns-2,unvme-ns-3"
+ARRAYNAME = "POSArray"
+
+
+def execute():
+    SCAN_DEV_BASIC.execute()
+    cli.mbr_reset()
+    out = cli.create_array("uram0", DATA, "", ARRAYNAME, "RAID6")
+    print(out)
+    return out
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        pos.set_addr(sys.argv[1])
+    api.clear_result(__file__)
+    out = execute()
+    ret = api.set_result_by_code_eq(out, 0, __file__)
+    pos.flush_and_kill_pos()
+    exit(ret)
