b00b43cd298df39a8a23c85bbfed96df795e0d9c
jh34.hong <jh34.hong@samsung.com>
2022-08-29 07:23:41 +0000

    [AWIBOF-6677] Fix array unit-test mockups
    Fix array fault-tolerance unit-test mockups.
    
    Update MOCK_METHOD of mockups.
    
    Signed-off-by: jh34.hong <jh34.hong@samsung.com>

--- test/unit-tests/array/ft/raid10_mock.h
+++ test/unit-tests/array/ft/raid10_mock.h
@@ -14,7 +14,7 @@ public:
     MOCK_METHOD(int, MakeParity, (list<FtWriteEntry>& ftl, const LogicalWriteEntry& src), (override));
     MOCK_METHOD(list<FtBlkAddr>, GetRebuildGroup, (FtBlkAddr fba, vector<uint32_t> abnormalDeviceIndex), (override));
     MOCK_METHOD(RecoverFunc, GetRecoverFunc, (int devIdx, vector<uint32_t> abnormalDeviceIndex), (override));
-    MOCK_METHOD(RaidState, GetRaidState, (vector<ArrayDeviceState> devs), (override));
+    MOCK_METHOD(RaidState, GetRaidState, (const vector<ArrayDeviceState>& devs), (override));
     MOCK_METHOD(vector<uint32_t>, GetParityOffset, (StripeId lsid), (override));
     MOCK_METHOD(bool, CheckNumofDevsToConfigure, (uint32_t numofDevs), (override));
     MOCK_METHOD((vector<pair<vector<uint32_t>, vector<uint32_t>>>), GetRebuildGroupPairs, (vector<uint32_t>& targetIndexs), (override));
