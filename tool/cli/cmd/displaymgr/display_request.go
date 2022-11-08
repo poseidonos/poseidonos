5875513936ef5401a43a6518ae0a88ffa1b88a8f
mjlee34 <jun20.lee@samsung.com>
2022-11-08 17:04:37 +0900

    [AWIBOF-7397] Add a method to display a protobuf CLI request in json
    Add a method to display a protobuf CLI request in json.
    
    Previously, the CLI client converts a CLI request in protobuf form
    to json form when specifying --json-req flag. The problem was
    that the readability was poor because the conversion was done
    in the high-level function that interpretes a command line.
    
    This commit adds PrintProtoReqJson(req protoreflect.ProtoMessage)
    that converts a protobuf request to json and display the request to stdout.
    
    Signed-off-by: mjlee34 <jun20.lee@samsung.com>

--- tool/cli/cmd/displaymgr/display_request.go
+++ tool/cli/cmd/displaymgr/display_request.go
@@ -3,6 +3,9 @@ package displaymgr
 import (
 	"cli/cmd/globals"
 	"fmt"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
 func PrintRequest(reqJson string) {
@@ -12,4 +15,23 @@ func PrintRequest(reqJson string) {
 
 		fmt.Println(reqHeader + reqJson + reqFooter)
 	}
+}
+
+func PrintProtoReqJson(req protoreflect.ProtoMessage) error {
+	if globals.IsJSONReq {
+		reqJson, err := protojson.MarshalOptions{
+			EmitUnpopulated: true,
+		}.Marshal(req)
+
+		if err != nil {
+			return err
+		}
+
+		reqHeader := `{"Request":`
+		reqFooter := `}`
+
+		fmt.Println(reqHeader + string(reqJson) + reqFooter)
+	}
+
+	return nil
 }
