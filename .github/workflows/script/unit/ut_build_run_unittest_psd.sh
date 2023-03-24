298c42c6e4cc95d42ea994e2ac0895a89772eebb
munseop.lim <munseop.lim@samsung.com>
2023-03-24 15:45:00 +0900

    [AWIBOF-8271] Add workflow for unit testing on pm
    Signed-off-by: munseop.lim <munseop.lim@samsung.com>

--- .github/workflows/script/unit/ut_build_run_unittest_psd.sh
+++ .github/workflows/script/unit/ut_build_run_unittest_psd.sh
@@ -1,11 +1,20 @@
 target_fabric_ip=127.0.0.1
 pos_working_dir="$1" #추후 수정
 
+sudo ${pos_working_dir}/script/pkgdep.sh
+
 echo "My IP..."
 ifconfig
 
 echo "My dir..."
 pwd
+
+echo "Install library"
+cd ${pos_working_dir}/lib/; sudo ./build_lib.sh
+
+echo "Copying event file..."
+sudo mkdir -p /etc/pos
+sudo cp ${pos_working_dir}/src/event/pos_event.yaml /etc/pos/pos_event.yaml
 
 echo "Running cmake..."
 cd ${pos_working_dir}/test/; sudo cmake .
@@ -16,12 +25,14 @@ if [ $retVal -ne 0 ]; then
 fi
 
 echo "Building test files..."
-cd ${pos_working_dir}/test/; sudo make -j 12
+cd ${pos_working_dir}/test/; sudo make -j$(nproc)
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Cannot proceed due to UT build error."
     exit $retVal
 fi
+
+echo 3 > /proc/sys/vm/drop_caches
 
 echo "Running UTs and ITs (a.k.a. called as basic_tests)"
 cd ${pos_working_dir}/test/; sudo make run_basic_tests
@@ -31,7 +42,7 @@ if [ $retVal -ne 0 ]; then
     exit $retVal
 fi
 
-echo "Calculating UT/IT code coverage..."
+echo "Calculating code coverage..."
 cd ${pos_working_dir}/test/; sudo make run_cov
 retVal=$?
 if [ $retVal -ne 0 ]; then
